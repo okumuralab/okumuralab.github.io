65ada1ac3efe81ba4a4cf2254eb5f3a93dadfd94
Haruhiko Okumura <okumura@okumuralab.org>
2020-02-19 16:10:40 +0900

    modified

---
+++ python/code/COVID-mhlw.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import numpy as np
+from statsmodels.stats.proportion import proportion_confint
+
+df = pd.read_csv("../data/COVID-mhlw.csv", index_col='Date', parse_dates=['Date'])
+
+locator = mdates.AutoDateLocator()
+formatter = mdates.ConciseDateFormatter(locator)
+ax = plt.gca()  # または ax = plt.subplot(1,1,1)
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+
+p = [x['Confirmed'] / x['Examined'] for i, x in df.iterrows()]
+ci0, ci1 = np.array([proportion_confint(x['Confirmed'], x['Examined'], method='beta')
+                     for i, x in df.iterrows()]).T
+ax.errorbar(df.index, p, [p - ci0, ci1 - p], fmt="o", capsize=5)
+
+plt.savefig('../img/COVID-mhlw.svg', bbox_inches="tight")
