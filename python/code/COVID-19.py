c8f88ba0431f4582d08bf457986d3d20316a9a06
Haruhiko Okumura <okumura@okumuralab.org>
2020-02-24 22:50:55 +0900

    modified

--- python/code/COVID-19.py
+++ python/code/COVID-19.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("../data/COVID-19.csv", index_col='Date', parse_dates=['Date'])
 
@@ -25,3 +26,17 @@ ax.plot(df.index, df['Global Deaths'], "s-")
 plt.yscale('log')
 plt.legend(['Confirmed', 'Deaths'])
 plt.savefig('../img/COVID-19-log.svg', bbox_inches="tight")
+
+plt.clf()
+locator = mdates.AutoDateLocator()
+formatter = mdates.ConciseDateFormatter(locator)
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+dt = (df.index.to_series().diff() / pd.Timedelta(days=1))
+c = df['Global Confirmed'].diff()
+c[df.index == '2020-02-17'] = np.nan
+ax.bar(df.index, c / dt, width=-dt, align='edge', edgecolor="black")
+ax.bar(df.index, df['Global Deaths'].diff() / dt, width=-dt, align='edge', edgecolor="black")
+plt.legend(['Confirmed', 'Deaths'])
+plt.savefig('../img/COVID-19-dif.svg', bbox_inches="tight")
