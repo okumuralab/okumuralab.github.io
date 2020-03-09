fdd493cb1fe26fe803eb6dc8bc8731c4c421b2c0
Haruhiko Okumura <okumura@okumuralab.org>
2020-03-09 13:51:52 +0900

    modified

--- python/code/COVID-mhlw.py
+++ python/code/COVID-mhlw.py
@@ -20,41 +20,6 @@ ax.legend(['Negative', 'Positive'])
 
 fig.savefig('../img/COVID-mhlw2.svg', bbox_inches="tight")
 
-# plt.clf()
-# locator = mdates.AutoDateLocator()
-# formatter = mdates.ConciseDateFormatter(locator)
-# ax = plt.gca()
-# ax.xaxis.set_major_locator(locator)
-# ax.xaxis.set_major_formatter(formatter)
-# dt = (df.index.to_series().diff() / pd.Timedelta(days=1))
-# ax.bar(df.index, df['Examined'].diff() / dt, width=-dt+0.1, align='edge')
-# ax.bar(df.index, df['Confirmed'].diff() / dt, width=-dt+0.1, align='edge')
-# plt.legend(['Negative', 'Positive'])
-# plt.savefig('../img/COVID-mhlw3.svg', bbox_inches="tight")
-
-# plt.clf()
-# locator = mdates.AutoDateLocator()
-# formatter = mdates.ConciseDateFormatter(locator)
-# ax = plt.gca()
-# ax.xaxis.set_major_locator(locator)
-# ax.xaxis.set_major_formatter(formatter)
-# dt = (df.index.to_series().diff() / pd.Timedelta(days=1))
-# ex = df['Examined'].diff() / dt
-# ex1 = ex.copy()
-# ex1['2020-03-04 12:00:00'] = 0
-# m = max(ex1.max()*1.1, 300)
-# plt.ylim(0, m)
-# ax.bar(df.index, ex, width=-dt+0.1, align='edge')
-# ax.bar(df.index, df['Confirmed'].diff() / dt, width=-dt+0.1, align='edge')
-# t = np.datetime64('2020-03-04 00:00:00', 'ns')
-# plt.text(t, m, '☁', # U+2601 (CLOUD)
-#          fontsize=60, color="lightgray",
-#          horizontalalignment='center', verticalalignment='center')
-# plt.text(t, m, int(ex['2020-03-04 12:00:00']),
-#          horizontalalignment='center', verticalalignment='center')
-# plt.legend(['Negative', 'Positive'])
-# plt.savefig('../img/COVID-mhlw3.svg', bbox_inches="tight")
-
 ax.clear()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
@@ -67,6 +32,9 @@ plt.ylim(0, m)
 ax.bar(df.index, ex, width=-dt+0.1, align='edge')
 ax.bar(df.index, df['Confirmed'].diff() / dt, width=-dt+0.1, align='edge')
 t = pd.Timestamp('2020-03-04 00:00')
+# ax.text(t, m, '☁', # U+2601 (CLOUD)
+#         fontsize=60, color="lightgray",
+#         horizontalalignment='center', verticalalignment='center')
 ax.text(t, m, int(ex['2020-03-04 12:00']), horizontalalignment='center')
 ax.legend(['Negative', 'Positive'])
 ax.plot([pd.Timestamp('2020-03-03 12:00'), pd.Timestamp('2020-03-04 12:00')],
