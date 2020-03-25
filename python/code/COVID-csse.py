6b384ea3c9d05ec8180aee6931fe1eac7d12fb0a
Haruhiko Okumura <okumura@okumuralab.org>
2020-03-25 14:41:52 +0900

    modified

--- python/code/COVID-csse.py
+++ python/code/COVID-csse.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import numpy as np
 from dateutil.parser import parse
 
-url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+# url = '/opt/local/GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 df = pd.read_csv(url)
 
@@ -38,7 +40,33 @@ fig.savefig('../img/COVID-csse.svg', bbox_inches="tight")
 
 #-----
 
-url2 = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+ax.clear()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+dx = np.diff(np.array(x, dtype=np.float), axis=0)
+dx[21][x[-1].index == 'China'] = np.nan
+ax.plot(t[1:], dx, 'o-')
+# ax.set_yscale('log')
+
+j = 0
+for i in x[-1].index:
+    if dx[-1][j] > 500:
+        ax.text(t[-1], dx[-1][j], i)
+    j += 1
+    
+# ax.legend(x[-1].index)
+
+japan = [x[i]['Japan'] for i in range(len(x))]
+ax.plot(t[1:], np.diff(japan), 'o-k', label='Japan')
+ax.set_ylabel('Confirmed')
+ax.legend()
+
+fig.savefig('../img/COVID-csse1.svg', bbox_inches="tight")
+
+#-----
+
+# url2 = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+url2 = '/opt/local/GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 
 df2 = pd.read_csv(url2)
 
@@ -64,3 +92,26 @@ ax.legend()
 
 fig.savefig('../img/COVID-csse2.svg', bbox_inches="tight")
 
+#-----
+
+ax.clear()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+dx = np.diff(np.array(x, dtype=np.float), axis=0)
+ax.plot(t[1:], dx, 'o-')
+# ax.set_yscale('log')
+
+j = 0
+for i in x[-1].index:
+    if dx[-1][j] > 20:
+        ax.text(t[-1], dx[-1][j], i)
+    j += 1
+    
+# ax.legend(x[-1].index)
+
+japan = [x[i]['Japan'] for i in range(len(x))]
+ax.plot(t[1:], np.diff(japan), 'o-k', label='Japan')
+ax.set_ylabel('Deaths')
+ax.legend()
+
+fig.savefig('../img/COVID-csse3.svg', bbox_inches="tight")
