58970e32b3d510af5d5788ad6d06a1c5e4e19b88
Haruhiko Okumura <okumura@okumuralab.org>
2020-04-12 22:55:42 +0900

    modified

--- python/code/COVID-tokyo.py
+++ python/code/COVID-tokyo.py
@@ -4,15 +4,26 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 
-df = pd.read_csv("../data/COVID-tokyo.csv", index_col='Date', parse_dates=['Date'])
+df = pd.read_csv("../data/COVID-tokyo.csv", parse_dates=['date'])
 
+fig, ax = plt.subplots()
 locator = mdates.AutoDateLocator()
 formatter = mdates.ConciseDateFormatter(locator)
-fig, ax = plt.subplots()
+
+ax.clear()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-dt = (df.index.to_series().diff() / pd.Timedelta(days=1))
-ax.bar(df.index, df['Examined'].diff() / dt, width=-dt, align='edge', edgecolor="white")
-ax.bar(df.index, df['Positive'].diff() / dt, width=-dt, align='edge', edgecolor="white")
-ax.legend(['Negative', 'Positive'])
-fig.savefig('../img/COVID-tokyo-dif.svg', bbox_inches="tight")
+ax.bar(df['date'], df['confirmed'], color='C1', width=1, align='edge', edgecolor="black")
+ax.legend(['Tokyo confirmed'], loc='upper left')
+fig.savefig('../img/COVID-tokyo.svg', bbox_inches="tight")
+
+ax.clear()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+ax.bar(df['date'], df['confirmed'].cumsum(), align='edge', label='Tokyo cumulative confirmed')
+mx = df['confirmed'].cumsum().values[-1]
+ax.axhline(mx, color='gray', linestyle='--', zorder=-1)
+ax.axhline(mx/2, color='gray', linestyle='--', zorder=-1)
+ax.legend(loc='upper left')
+fig.savefig('../img/COVID-tokyo-cum.svg', bbox_inches="tight")
+
