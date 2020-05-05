dd4fa43752b75e6a2ab33ae1b9ae5ad39e1d369b
Haruhiko Okumura <okumura@okumuralab.org>
2020-05-06 08:24:39 +0900

    modified

--- python/code/COVID-world.py
+++ python/code/COVID-world.py
@@ -19,49 +19,24 @@ else:
     url = url + a[0]
 
 dic = {
-    'Republic of)': 'Iran',
-    'of)': 'Iran',
-    'Iran (Islamic': 'Iran',
-    'Iran (Islamic Republic of)': 'Iran',
-    'Iran (Islamic Republic': 'Iran',
-    'Emirates': 'Arab',
-    'United Arab': 'Arab',
-    'United Arab Emirates': 'Arab',
-    'Occupied': 'Palestine',
-    'Palestinian Territory': 'Palestine',
-    'occupied Palestinian': 'Palestine',
-    'territory': 'Palestine',
-    'United States of': 'US',
-    'the United States': 'US',
     'United States of America': 'US',
-    'America': 'US',
     'Republic of Korea': 'Korea',
-    'the United': 'UK',
-    'the United Kingdom': 'UK',
     'The United Kingdom': 'UK',
-    'of the Congo': 'Congo',
-    'State of)': 'Bolivia',
-    'Herzegovina': 'Bosnia',
-    'Bosnia and': 'Bosnia',
-    'International': '(Diamond Princess)',
-    'conveyance': '(Diamond Princess)',
-    'International conveyance': '(Diamond Princess)',
-    'Saint Barthélemy': 'Saint Barthelemy',
+    'United Arab Emirates': 'Arab',
+    'occupied Palestinian territory': 'Palestine',
+    'Bosnia and Herzegovina': 'Bosnia',
+    'Russian Federation': 'Russia',
+    'Republic of Moldova': 'Moldova',
+    'Iran (Islamic Republic of)': 'Iran'
 }
 
 countries = []
 confirmed = []
 death = []
 
-prev = ''
 with urllib.request.urlopen(url) as f:
     for line in "".join(pdftotext.PDF(f)).split("\n"):
         line = line.strip()
-        # m = re.search(r'^Total +(\d+) +(\d+) +(\d+) +(\d+) +(\d+) +(\d+)$', line)
-        # if m:
-        #     countries.append('China')
-        #     confirmed.append(int(m[5]))
-        #     death.append(int(m[6]))
         m = re.search(r'^([^\d]+)?(\d+) +(-?\d+) +(\d+) +(-?\d+) +([A-Za-z ]+)(\d+)$', line)
         if m and m[1] and m[1].strip() != 'Grand total':
             print(line)
@@ -71,7 +46,6 @@ with urllib.request.urlopen(url) as f:
             countries.append(country)
             confirmed.append(int(m[2]))
             death.append(int(m[4]))
-        prev = line
 
 countries = np.array(countries)
 confirmed = np.array(confirmed)
@@ -81,17 +55,14 @@ plt.figure(figsize=[6.4, 6.4])
 
 bottom = 100
 
-# plt.clf()
+plt.clf()
 plt.plot(confirmed[death >= bottom], death[death >= bottom], 'o')
 plt.xscale('log')
 plt.yscale('log')
 plt.axis('equal')
 
 for x in zip(confirmed[death >= bottom], death[death >= bottom], countries[death >= bottom]):
-    if len(x[2]) == 2 or len(x[2]) == 5:
-        plt.text(x[0], x[1], x[2], horizontalalignment='right', verticalalignment='top')
-    else:
-        plt.text(x[0], x[1], x[2], horizontalalignment='left', verticalalignment='bottom')
+    plt.text(x[0], x[1], x[2], horizontalalignment='left', verticalalignment='bottom')
 
 plt.xlabel('Confirmed')
 plt.ylabel('Deaths')
