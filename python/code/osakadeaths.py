68b5735991bd53eaedad19584d3fb17bb6f0ac28
Haruhiko Okumura <okumura@okumuralab.org>
2020-05-26 08:09:31 +0900

    added

---
+++ python/code/osakadeaths.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('../data/osakadeaths.csv')
+
+def days(year, month):
+    m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return m[month - 1] + ((month == 2) and
+                           ((year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0)))
+
+perday = np.array([r[2] / days(r[0], r[1]) for i, r in df.iterrows()])
+
+plt.clf()
+for y in range(2015, 2021):
+    plt.plot(df[df['year'] == y]['month'], perday[df['year'] == y], 'o-', label=y)
+
+plt.legend()
+plt.savefig('../img/osakadeaths.svg', bbox_inches="tight")
