c98750a899cdb705a3036fae2fc78e68c4a9ee85
Haruhiko Okumura <okumura@okumuralab.org>
2020-05-30 18:52:52 +0900

    modified

--- python/code/200312.py
+++ python/code/200312.py
@@ -119,3 +119,36 @@ ax.legend(['発症日 (onset)', '確定日 (confirmed)'])
 
 fig.savefig('../img/200312h.svg', bbox_inches="tight")
 
+exit()
+
+#-----
+
+df1 = df[df['受診都道府県'] != '東京都']
+# df1 = df[(df['受診都道府県'] != '東京都') & (df['発症日'] == datetime.datetime(2020,4,1))]
+
+ax.clear()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+
+ax.hist(df1['発症日'], bins=b, edgecolor="black", alpha=0.5)
+ax.hist(df1['確定日'], bins=b, edgecolor="black", alpha=0.5)
+ax.set_xlim(datetime.datetime(2020,3,20), datetime.datetime(2020,5,1))
+ax.legend(['発症日 (onset)', '確定日 (confirmed)'])
+
+fig.savefig('../img/200312i.svg', bbox_inches="tight")
+
+#-----
+
+ken = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+
+for k in ken:
+    ax.clear()
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+    df1 = df[df['受診都道府県'] == k]
+    ax.hist(df1['発症日'], bins=b, edgecolor="black", alpha=0.5)
+    ax.hist(df1['確定日'], bins=b, edgecolor="black", alpha=0.5)
+    ax.legend(['発症日 (onset)', '確定日 (confirmed)'])
+    fig.savefig(k + '.svg', bbox_inches="tight")
+
+#-----
