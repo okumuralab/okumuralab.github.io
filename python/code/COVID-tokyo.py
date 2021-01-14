5fa239283bbf60fbb5220ca090b4ab5cb4e14789
okumuralab <okumura@okumuralab.org>
2021-01-14 16:58:46 +0900

    modified

--- python/code/COVID-tokyo.py
+++ python/code/COVID-tokyo.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 import datetime
+import os
 
 df = pd.read_csv("../data/COVID-tokyo.csv", parse_dates=['date'])
 # now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
-now = f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}'
+# now = f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}'
+p = os.stat("../data/COVID-tokyo.csv")
+# now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
+now = str(datetime.datetime.fromtimestamp(p.st_mtime))
 
 fig, ax = plt.subplots()
 locator = mdates.AutoDateLocator()
@@ -72,3 +76,35 @@ w = sum(w, [])
 h1, h2 = np.histogram(w, range(0,8))
 ax.bar(['月','火','水','木','金','土','日'], h1, color="lightgray", edgecolor="black")
 ax.set_xlabel('確定日')
+
+exit()
+
+fig, ax = plt.subplots()
+locator = mdates.AutoDateLocator()
+formatter = mdates.ConciseDateFormatter(locator)
+fig.text(0.9, 0.89, 'generated: ' + now, horizontalalignment='right')
+
+cmap = plt.get_cmap('tab20')
+# col = ['C0', 'C0', 'C0', 'C0', 'C0', 'C1', 'C1']
+col = [cmap(3), cmap(3), cmap(3), cmap(3), cmap(3), cmap(2), cmap(2)]
+cols = [col[pd.Timestamp(i).dayofweek] for i in df['date'].values]
+
+ax.clear()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+ax.bar(df['date'], df['confirmed'], color=cols, width=1,
+       align='edge',
+       edgecolor="black", linewidth=0.5)
+ax.set_xlim(pd.to_datetime('2020-12-01'), df['date'].values[-1] + pd.to_timedelta(1, 'day'))
+# ax.legend(['Tokyo confirmed'], loc='upper left')
+
+ax2 = ax.twinx()
+ax2.set_ylim(ax.get_ylim())
+ax2.set_yticks([df['confirmed'].values[-1]])
+
+# ax_pos = ax.get_position()
+# fig.text(ax_pos.x1 + 0.01,
+#          (df['confirmed'].values[-1] / ax.get_ylim()[1]) *  (ax_pos.y1 - ax_pos.y0) + ax_pos.y0,
+#          df['confirmed'].values[-1], verticalalignment='center')
+
+fig.savefig('../img/COVID-tokyo.svg', bbox_inches="tight")
