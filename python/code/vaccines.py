5f4bd0d9d02db67515c52ab635c6ca3e6f6d2f7e
okumuralab <okumura@okumuralab.org>
2021-04-20 19:52:57 +0900

    modified

--- python/code/vaccines.py
+++ python/code/vaccines.py
@@ -1,9 +1,35 @@
 #! /usr/bin/env python3
 
+import subprocess
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("../data/vaccines.csv", parse_dates=['日付'])
+URL2 = "https://www.kantei.go.jp/jp/content/IRYO-vaccination_data.xlsx"
+URL3 = "https://www.kantei.go.jp/jp/content/KOREI-vaccination_data.xlsx"
+
+def wget(url):
+    r = subprocess.run(["wget", "-N", url],
+                       capture_output=True, text=True,
+                       env={"PATH": "/usr/local/bin:/usr/bin", "LANG": "en"})
+    return " saved " in r.stderr
+
+t2 = wget(URL2)
+t3 = wget(URL3)
+if not (t2 or t3):
+    print("No new data")
+    exit()
+
+df1 = pd.read_csv("../data/vaccines.csv", parse_dates=['日付'])
+df2 = pd.read_excel("IRYO-vaccination_data.xlsx",
+                   header=None, skiprows=4,
+                   usecols="A,D,E").dropna()
+df3 = pd.read_excel("KOREI-vaccination_data.xlsx",
+                   header=None, skiprows=4,
+                   usecols="A,D,E").dropna()
+df4 = pd.merge(df2, df3, how="outer", on=0).fillna(0)
+df4.columns = ['日付', '医療従事者1回目', '医療従事者2回目',
+               'その他1回目', 'その他2回目']
+df = pd.merge(df1, df4, how="outer").fillna(0)
 
 bottom = 0
 for c in df.columns[1:]:
