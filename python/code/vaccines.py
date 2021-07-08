d5e889b87a49b4c417f22012ad479524b3cb5298
Haruhiko Okumura <okumura@okumuralab.org>
2021-07-08 21:24:48 +0900

    modified

--- python/code/vaccines.py
+++ python/code/vaccines.py
@@ -7,8 +7,9 @@ import subprocess
 import matplotlib.pyplot as plt
 import pandas as pd
 
-URL2 = "https://www.kantei.go.jp/jp/content/IRYO-vaccination_data.xlsx"
-URL3 = "https://www.kantei.go.jp/jp/content/KOREI-vaccination_data.xlsx"
+# URL2 = "https://www.kantei.go.jp/jp/content/IRYO-vaccination_data3.xlsx"
+# URL3 = "https://www.kantei.go.jp/jp/content/KOREI-vaccination_data3.xlsx"
+URL = "https://www.kantei.go.jp/jp/content/vaccination_data5.xlsx"
 
 def filename(url):
     return re.sub("^.*/", "", url)
@@ -18,33 +19,37 @@ def wget(url):
         t = os.stat(filename(url)).st_mtime
     except:
         t = 0
-    if time.time() - t < 3600 * 20:  # 20時間より新しければ再取得しない
+    if time.time() - t < 3600 * 10:  # 10時間より新しければ再取得しない
         return False
     r = subprocess.run(["wget", "-N", url],
                        capture_output=True, text=True,
                        env={"PATH": "/usr/local/bin:/usr/bin", "LANG": "en"})
     return " saved " in r.stderr
 
-t2 = wget(URL2)
-t3 = wget(URL3)
-if not (t2 or t3):
+t = wget(URL)
+if not t:
     exit()
 
 df1 = pd.read_csv("../data/vaccines.csv", parse_dates=['日付'])
-df2 = pd.read_excel("IRYO-vaccination_data.xlsx",
-                   header=None, skiprows=4,
-                   usecols="A,D,E").dropna()
-df3 = pd.read_excel("KOREI-vaccination_data.xlsx",
-                   header=None, skiprows=4,
-                   usecols="A,D,E").dropna()
+df2 = pd.read_excel("vaccination_data5.xlsx", sheet_name="医療従事者等",
+                   header=None, skiprows=5,
+                   usecols="A,D,E,F,G").dropna(thresh=3).fillna(0)
+df3 = pd.read_excel("vaccination_data5.xlsx", sheet_name="一般接種",
+                   header=None, skiprows=6,
+                   usecols="A,D,E,F,G").dropna(thresh=3).fillna(0)
+df2.iloc[:, 1] += df2.iloc[:, 2]
+df2.iloc[:, 3] += df2.iloc[:, 4]
+df3.iloc[:, 1] += df3.iloc[:, 2]
+df3.iloc[:, 3] += df3.iloc[:, 4]
 df4 = pd.merge(df2, df3, how="outer", on=0).fillna(0)
-df4.columns = ['日付', '医療従事者1回目', '医療従事者2回目',
-               'その他1回目', 'その他2回目']
+df4.columns = ['日付', '医療従事者1回目', 'A', '医療従事者2回目', 'B',
+               'その他1回目', 'C', 'その他2回目', 'D']
 df = pd.merge(df1, df4, how="outer").fillna(0)
+df.columns = ['日付', '医療従事者等1回目', '医療従事者等2回目', '一般接種1回目', '一般接種2回目', 'A', 'B', 'C', 'D']
 
 bottom = 0
-for c in df.columns[1:]:
-    plt.bar(df['日付'], df[c], bottom=bottom, label=c)
+for c in df.columns[[4, 3, 2, 1]]:
+    plt.bar(df['日付'], df[c], bottom=bottom, label=c) # edgecolor="white", linewidth=0.1
     bottom += df[c]
 plt.legend()
 plt.xticks(rotation=20)
