e180d89b0622667b6c27fa7165fe4f59eec53bc6
Haruhiko Okumura <okumura@okumuralab.org>
2022-02-13 15:28:14 +0900

    modified

--- python/code/COVID-mhlwopen.py
+++ python/code/COVID-mhlwopen.py
@@ -7,60 +7,49 @@ import time
 
 def readcsv(url):
     try:
-        df = pd.read_csv(url, parse_dates=['日付'])
+        df = pd.read_csv(url, parse_dates=[0])
         print('utf-8:', url)
     except UnicodeDecodeError:
-        df = pd.read_csv(url, parse_dates=['日付'], encoding='cp932')
+        df = pd.read_csv(url, parse_dates=[0], encoding='cp932')
         print('cp932:', url)
     return df
 
 now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
 
-# PCR検査陽性者数
-# df1 = readcsv("https://www.mhlw.go.jp/content/pcr_positive_daily.csv")
-df1 = pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv", parse_dates=['Date'])
-# df1 = df1[df1.Prefecture == "ALL"]
+# PCR検査実施人数
+df1 = readcsv("https://www.mhlw.go.jp/content/pcr_tested_daily.csv")
 
-# PCR 検査実施件数(単日)
-df2 = readcsv("https://www.mhlw.go.jp/content/pcr_tested_daily.csv")
+# 新規陽性者数の推移（日別）
+df2 = readcsv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
 
-# 死亡者数
-# df5 = readcsv("https://www.mhlw.go.jp/content/death_total.csv")
-df5 = pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/deaths_cumulative_daily.csv", parse_dates=['Date'])
-# df5 = df5[df5.Prefecture == "ALL"]
+# 死亡者数（累積）
+df3 = readcsv("https://covid19.mhlw.go.jp/public/opendata/deaths_cumulative_daily.csv")
+
+fig, ax = plt.subplots()
+fig.text(0.9, 0.89, 'generated: ' + now, horizontalalignment='right')
 
 locator = mdates.AutoDateLocator()
 formatter = mdates.ConciseDateFormatter(locator)
 
-fig, ax = plt.subplots()
-ax.xaxis.set_major_locator(locator)
-ax.xaxis.set_major_formatter(formatter)
-fig.text(0.9, 0.89, 'generated: ' + now, horizontalalignment='right')
-
 ax.clear()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-# ax.bar(df2['日付'], df2['PCR 検査実施人数(単日)'], width=1)
-ax.bar(df2.iloc[:,0], df2.iloc[:,1], width=1)
-# ax.bar(df1['Date'], df1['Newly confirmed cases'], width=1)
-ax.bar(df1['Date'], df1['ALL'], width=1)
+ax.bar(df1.iloc[:,0], df1.iloc[:,1], width=1)
+ax.bar(df2['Date'], df2['ALL'], width=1)
 ax.legend(['Negative', 'Positive'], loc='upper left')
 fig.savefig('../img/COVID-mhlwopen1.svg', bbox_inches="tight")
 
 ax.clear()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-# ax.bar(df1['Date'], df1['Newly confirmed cases'], width=1, color='C1')
-ax.bar(df1['Date'], df1['ALL'], width=1, color='C1')
-# ax.bar(df5['Date'], df5['Deaths(Cumulative)'].diff(), width=1, color='C3')
-ax.bar(df5['Date'], df5['ALL'].diff(), width=1, color='C3')
+ax.bar(df2['Date'], df2['ALL'], width=1, color='C1')
+ax.bar(df3['Date'], df3['ALL'].diff(), width=1, color='C3')
 ax.legend(['Positive', 'Deaths'], loc='upper left')
 fig.savefig('../img/COVID-mhlwopen2.svg', bbox_inches="tight")
 
 ax.clear()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-# ax.bar(df5['Date'], df5['Deaths(Cumulative)'].diff(), width=1, color='C3')
-ax.bar(df5['Date'], df5['ALL'].diff(), width=1, color='C3')
+ax.bar(df3['Date'], df3['ALL'].diff(), width=1, color='C3')
 ax.legend(['Deaths'], loc='upper left')
 fig.savefig('../img/COVID-mhlwopen3.svg', bbox_inches="tight")
