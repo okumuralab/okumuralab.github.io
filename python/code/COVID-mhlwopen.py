09358243480bd21b38e88a8c99f8a61c701be4ae
okumuralab <okumura@okumuralab.org>
2022-12-08 21:51:22 +0900

    update

--- python/code/COVID-mhlwopen.py
+++ python/code/COVID-mhlwopen.py
@@ -4,23 +4,65 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 import time
+import json
+import twitter
+import os
 
 def readcsv(url):
     try:
         df = pd.read_csv(url, parse_dates=[0], thousands=",")
         print('utf-8:', url)
     except UnicodeDecodeError:
-        df = pd.read_csv(url, parse_dates=[0], encoding='cp932')
+        df = pd.read_csv(url, parse_dates=[0], thousands=",", encoding='cp932')
         print('cp932:', url)
     return df
 
 now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
+nowdate = time.strftime('%Y-%m-%d', time.localtime())
+
+# 新規陽性者数の推移（日別）
+for i in range(60):
+    df2 = readcsv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
+    if pd.Timestamp(df2['Date'].values[-1]).strftime("%Y-%m-%d") >= nowdate:
+        break
+    time.sleep(60)  # wait 1 minute
+
+if pd.Timestamp(df2['Date'].values[-1]).strftime("%Y-%m-%d") < nowdate:
+    print("Timed out")
+    exit()
+
+confirmed = df2['Tokyo'].values[-1]
+MYURL = "https://okumuralab.org/~okumura/python/COVID-19.html"
+status = f"今日の東京は{confirmed}人 {MYURL}"
+print(status)
+FILE = "/home/okumura/public_html/python/data/COVID-tokyo.csv"
+with open(FILE, "a") as f:
+    f.write(f"{nowdate},{confirmed}\n")
+os.system("cd /home/okumura/public_html/python/code && ./COVID-tokyo.py")
+with open("/home/okumura/.twitter.json") as f:
+    token = json.load(f)
+api = twitter.Api(consumer_key=token["consumer_key"],
+                  consumer_secret=token["consumer_secret"],
+                  access_token_key=token["access_token"],
+                  access_token_secret=token["access_token_secret"],
+                  tweet_mode='extended',
+                  sleep_on_rate_limit=True)
+# ツイート
+r = api.PostUpdate(
+    status, # ツイート本文
+    media=[
+        "/home/okumura/public_html/python/img/COVID-tokyo-a.png",
+        "/home/okumura/public_html/python/img/COVID-tokyo-rt.png"
+    ],
+    latitude=34.7468, longitude=136.5248, # 位置情報
+    display_coordinates=True # 正確な位置情報にする
+)
+print(f"https://twitter.com/h_okumura/status/{r.id}")
+os.system("rsync -e 'ssh -p 59224' -auvz /home/okumura/public_html/python/data/COVID-tokyo.csv oku.edu.mie-u.ac.jp:public_html/python/data/")
+
 
 # PCR検査実施人数
 df1 = readcsv("https://www.mhlw.go.jp/content/pcr_tested_daily.csv")
-
-# 新規陽性者数の推移（日別）
-df2 = readcsv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
 
 # 死亡者数（累積）
 df3 = readcsv("https://covid19.mhlw.go.jp/public/opendata/deaths_cumulative_daily.csv")
